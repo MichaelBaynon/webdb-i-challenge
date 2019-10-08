dfba0e035de84bbd435e2801e2d6ec165899660c
MichaelBaynon <baynonmichael@gmail.com>
2019-10-07 20:49:27 -0400

    wip2

--- routes/accounts/accountsRouter.js
+++ routes/accounts/accountsRouter.js
@@ -1,10 +1,48 @@
-const router = require('express').Router()
-const db = require('../../data/dbConfig')
+const router = require("express").Router();
+const db = require("../../data/dbConfig");
 
-router.get('/', (req, res) => {
-db('*').from('accounts').then(accounts => {
-    res.send(accounts)
-})
-})
+router.get("/", (req, res) => {
+  db("*")
+    .from("accounts")
+    .then(accounts => {
+      res.send(accounts);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
 
-module.exports = router
+router.get("/:id", (req, res) => {
+  db.select("*")
+    .from("accounts")
+    .where("id", "=", req.params.id)
+    .then(account => {
+      res.send(account);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+router.post("/", (req, res) => {
+  const accountData = req.body;
+
+  db("accounts")
+    .insert(accountData, "id")
+    .then(ids => {
+      res.json(ids);
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+router.delete("/:id", (req, res) => {
+  db("*")
+    .from("accounts")
+    .then(accounts => {
+      res.delete(accounts);
+    });
+});
+
+module.exports = router;
